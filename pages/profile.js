93f63b31bb29f03824ebca1aa342067fcaf953e9
김태이 <taei@taei-MacBookPro.local>
2021-06-13 20:51:42 +0900

    팔로워 리스트까지 완성

--- pages/profile.js
+++ pages/profile.js
@@ -2,14 +2,30 @@ import Head from "next/head";
 import React from "react";
 
 import AppLayout from "../components/Applayout";
+import FollowList from "../components/FollowList";
+import NicknameEditForm from "../components/NicknameEditForm";
 
 const Profile = () => {
+  const followerList = [
+    { nickname: "태이" },
+    { nickname: "ㅁㅁ" },
+    { nickname: "노드버드" },
+  ];
+  const followingList = [
+    { nickname: "태이" },
+    { nickname: "ㅁㅁ" },
+    { nickname: "노드버드" },
+  ];
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
